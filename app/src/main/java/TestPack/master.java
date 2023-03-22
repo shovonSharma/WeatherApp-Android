ea96189291d9fe2dfaf9bbf8aa7f34d7b30313ac
sharma shovon <sharmashovon875@gmail.com>
2023-03-23 00:25:50 +0600

    learn purpose getter setter added

---
+++ app/src/main/java/TestPack/master.java
@@ -0,0 +1,22 @@
+package TestPack;
+
+public class master {
+    private int x;
+    private string str;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public string getStr() {
+        return str;
+    }
+
+    public void setStr(string str) {
+        this.str = str;
+    }
+}
